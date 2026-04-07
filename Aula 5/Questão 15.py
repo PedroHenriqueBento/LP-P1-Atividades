5c57a38091a2531b836fe60b6493468e57668190
Pedro Henrique <pedro.ryan8090@hotmail.com>
2026-04-07 02:48:44 -0300

    _

---
+++ Aula 5/Questão 15.py
@@ -0,0 +1,6 @@
+#Leia um número e: Se estiver entre 10 e 20 → “Dentro”; Caso contrário → “Fora”.
+numero = int(input("Digite seu número"))
+if numero > 10 and numero < 20:
+  print("Dentro")
+else:
+  print("Fora")
