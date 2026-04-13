d5a4bde8f6d65720cb8c6f6f476de2e0150d759d
Pedro Henrique <pedro.ryan8090@hotmail.com>
2026-04-13 19:04:55 -0300

    Create questão 20.py

---
+++ Aula 5/questão 20.py
@@ -0,0 +1,6 @@
+valor = float(input("Digite um valor: "))
+
+if 0 <= valor <= 100:
+    print(f"O valor {valor} está dentro do intervalo de 0 a 100!")
+else:
+    print(f"O valor {valor} está fora do intervalo!")
