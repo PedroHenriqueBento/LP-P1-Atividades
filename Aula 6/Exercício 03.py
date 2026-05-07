2bc6b554c58c147b4d3cd4062a973eb322e40812
Pedro Henrique <pedro.ryan8090@hotmail.com>
2026-05-07 15:54:26 -0300

    Faça um programa que calcule o fatorial de um número inteiro fornecido pelo usuário. Ex.: 5!=5.4.3.2.1=120

---
+++ Aula 6/Exercício 03.py
@@ -0,0 +1,8 @@
+numero = int(input("Entre com um número inteiro positivo: "))
+fatorial = 1
+
+while numero > 0:
+    fatorial = fatorial * n
+    numero = numero - 1
+
+print(f"O fatorial é: {fatorial}")
